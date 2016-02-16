3710eb8cd0d48ba1c565105f5a6d3490305a1507
Nina <ninaolo@kth.se>
2016-02-16 14:55:13 +0100

    Add instructions view javascript

---
+++ js/view/instructionsView.js
@@ -0,0 +1,16 @@
+function loadDish(container, dish) {
+	container.find("#dishPic").attr("src", "images/" + dish.image);
+	container.find("#dishName").append(dish.name);
+	container.find("#dishInstructions").append(dish.description);
+}
+
+var InstructionsView = function (container, model) {
+
+	container.load("fragments/instructions.html", function() {
+		this.menu = model.getFullMenu();
+		loadDish(container.find("#starter"), this.menu[0]);
+		loadDish(container.find("#main"), this.menu[1]);
+		loadDish(container.find("#dessert"), this.menu[2]);
+	});
+	
+}
