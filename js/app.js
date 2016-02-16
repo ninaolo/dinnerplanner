c15c937b7a1142414314fd310a3d2b9f0d25e4a8
Li <liwi@kth.se>
2016-02-16 13:59:23 +0100

    added new view backAndEdit

--- js/app.js
+++ js/app.js
@@ -6,18 +6,20 @@ $(function() {
 	var selectDishView = new SelectDishView($("#selectDishView"), model);
 	var homeView = new HomeView($("#homeView"), model);
 	var dishDetailsView = new DishDetailsView($("#dishDetailsView"), model);
-
+	var backAndEditView = new BackAndEditView ($("backAndEditView"), model);
+	
 	// hide() and show() will later be fixed with controllers
 	$("#homeView").hide();
 	$("#sidebarView").hide();
 	$("#selectDishView").hide();
 	$("#dishDetailsView").hide();
+	$("backAndEditView").hide();
 
 
 
-	// SCREEN 1 - HOME
-
-	$("#homeView").show();
+	// SCREEN 1 - HOM
+	
+	//$("#homeView").show();
 
 	// SCREEN 2 - SELECT DISH
 
@@ -28,5 +30,9 @@ $(function() {
 
 	//$("#sidebarView").show();
 	//$("#dishDetailsView").show();
+
+	// SCREEN 5 - DINNER OVERVIEW 
+
+	$("#backAndEditView").show();
 
 });
