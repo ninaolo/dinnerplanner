094296eb2378f230a13105551534c01d2f3c252c
Nina <ninaolo@kth.se>
2016-02-23 21:29:23 +0100

    Fix dish details view

--- js/controller/dishDetailsController.js
+++ js/controller/dishDetailsController.js
@@ -4,12 +4,12 @@ var DishDetailsController = function(dishDetailsView,selectDishView, dinnerModel
     dishDetailsView.confirmDish.click(function(){
         dinnerModel.addDishToMenu(dishDetailsView.selectedDish);
         dishDetailsView.container.hide();
-        selectedDishView.container.show();
+        selectDishView.container.show();
     });
 
     dishDetailsView.backSelectDish.click(function(){
         dishDetailsView.container.hide();
-        selectedDishView.container.show();
+        selectDishView.container.show();
     });
 
 }
