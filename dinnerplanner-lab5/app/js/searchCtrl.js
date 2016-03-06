af1812ace90cffffb2d72351ad4660056ce6cedb
Nina <ninaolo@kth.se>
2016-03-06 19:41:03 +0100

    Make select dish type work

--- dinnerplanner-lab5/app/js/searchCtrl.js
+++ dinnerplanner-lab5/app/js/searchCtrl.js
@@ -5,14 +5,19 @@ dinnerPlannerApp.controller('SearchCtrl', function ($scope,Dinner) {
   // TODO in Lab 5: you will need to implement a method that searchers for dishes
   // including the case while the search is still running.
 
-    $scope.search = function(query) {
+    $scope.types = Dinner.getDishTypes();
+
+    $scope.search = function(query, dishType) {
         $scope.status = "Searching...";
-        Dinner.DishSearch.get({title_kw:query},function(data){
+        var titleSearch = (query == undefined ? "" : query) + " " + (dishType == undefined ? "" : dishType);
+        console.log(titleSearch);
+        Dinner.DishSearch.get({title_kw:titleSearch}, function(data){
             $scope.dishes=data.Results;
             $scope.status = "Showing " + data.Results.length + " results.";
         },function(data){
             $scope.status = "There was an error";
         });
     }
+
 
 });
