5dd1aa51d844d144382b26741f5b83f879c99455
Li <liwi@kth.se>
2016-03-08 22:05:35 +0100

    included OverviewCtrl and instructionsCtrl. Also added functionality to instructions.html and instuctionsCtrl

--- dinnerplanner-lab5/app/js/InstructionsCtrl.js
+++ dinnerplanner-lab5/app/js/InstructionsCtrl.js
@@ -1,3 +1,10 @@
 /**
  * Created by Nina on 2016-03-06.
  */
+
+dinnerPlannerApp.controller('InstructionsCtrl', function ($scope,Dinner) {
+
+    $scope.menu = Dinner.getFullMenu();
+
+});
+
--- dinnerplanner-lab5/app/js/OverviewCtrl.js
+++ dinnerplanner-lab5/app/js/OverviewCtrl.js
@@ -3,4 +3,10 @@ dinnerPlannerApp.controller('OverviewCtrl', function ($scope,Dinner) {
 
     $scope.menu = Dinner.getFullMenu();
 
+    $scope.totalMenuPrice = 0;
+    $scope.totalMenuPrice = Dinner.getTotalMenuPrice();
+
+    $scope.getTotalDishPrice = function(dish) {
+        return Dinner.getTotalDishPrice(dish);
+    }
 });
